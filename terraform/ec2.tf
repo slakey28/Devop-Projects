3fd0f243e386110ec631c41d92d7203731e93625
slakey28 <slakey28@outlook.com>
2026-06-15 14:37:46 +0000

    Added Terraform files

---
+++ terraform/ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "web" {
+
+  name = "web-sg"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "YOUR_PUBLIC_IP/32"
+    ]
+  }
+
+  ingress {
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+
+  description = "EC2 instance size"
+
+  default = "t3.micro"
+
+}
