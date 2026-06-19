d62021ceceeb37246f5247b8a07bc4ca7785425e
slakey28 <slakey28@outlook.com>
2026-06-19 10:54:33 +0000

    Add GitHub Actions DevSecOps pipeline

---
+++ terraform/eip.tf
@@ -0,0 +1,8 @@
+resource "aws_eip" "app" {
+  instance = aws_instance.app.id
+  domain   = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-eip"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app.public_ip
+  description = "Elastic (static) public IP address of the EC2 instance"
+  value       = aws_eip.app.public_ip
 }
 
 output "instance_id" {
@@ -10,5 +10,5 @@ output "instance_id" {
 
 output "ssh_command" {
   description = "Convenience SSH command"
-  value       = "ssh -i ~/.ssh/id_ed25519.pub ubuntu@${aws_instance.app.public_ip}"
+  value       = "ssh -i ~/.ssh/id_ed25519 ubuntu@${aws_eip.app.public_ip}"
 }
